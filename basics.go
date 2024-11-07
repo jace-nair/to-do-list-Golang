d9eeabc41d6f16905aaeb17faf055b3575ffd594
Jace <contactjacenair@gmail.com>
2024-11-07 11:00:15 +0000

    Add basics.go file showin all he data types used

---
+++ basics.go
@@ -0,0 +1,19 @@
+package main
+
+const myConstInt int = 2
+const myCosntString string = "This is a constant string"
+
+var myVarInt int = 3
+var myVarString string = "This is a variable string"
+
+var myFloat float64 = 2.33
+
+var myStringArray [3]string = [3]string{"string1", "string2", "string3"}
+var myIntArray [3]int = [3]int{1, 2, 3}
+
+var myStringSlice []string = []string{"string1", "string2", "string3", "string4", "string5"}
+var myIntSlice []int = []int{1, 2, 3, 4, 5}
+
+var myStringMap map[string]string = map[string]string{"Name": "Henry", "Occupation": "Student", "Status": "Single"}
+var myIntMap map[int]int = map[int]int{1: 100, 2: 200, 3: 300}
+var myIntStringMap map[int]string = map[int]string{1: "Jack", 2: "Henry", 3: "David"}
